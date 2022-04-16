3e958034b0b1c2a26a853a9343389b8fe85f502d
m-moharrami <moharram.m98@gmail.com>
2022-04-16 23:15:54 +0430

    put the data inside html

--- jeopardy/code.js
+++ jeopardy/code.js
@@ -3,3 +3,20 @@ async function getCategories() {
     let data = await response.json();
     return data;
 }
+
+function getCategoryHtml(category) {
+    return `
+        <div class="my-category-title">${category.title}</div>
+        <div class="my-category-clue">$100</div>
+        <div class="my-category-clue">$200</div>
+        <div class="my-category-clue">$300</div>
+        <div class="my-category-clue">$400</div>
+    `;
+}
+
+getCategories().then(categories => {
+    console.log(categories);
+    document.body.innerHTML = `
+        <div class="board">${categories.map(getCategoryHtml).join('')}</div>
+    `;
+})
